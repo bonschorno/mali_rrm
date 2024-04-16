46969a7434e4a07b66cffd8ffc21a52a8976bb79
bonschorno <colin.walder@hotmail.com>
2024-04-16 10:42:59 +0200

    Add dictionary.

--- data-raw/data_processing.R
+++ data-raw/data_processing.R
@@ -1,3 +1,21 @@
-## code to prepare `DATASET` dataset goes here
+library(tidyverse)
+library(janitor)
 
-usethis::use_data(DATASET, overwrite = TRUE)
+# import data -------------------------------------------------------------
+
+data_in <- readxl::read_xlsx("data-raw/MLI-I-REACH-I-ABA-I-Base-de-Donnees-I-Medina-Coura-Chateau-Sosso-Koira-.xlsx",
+                             sheet = "raw") %>%
+  select(1:10) %>%
+  clean_names()
+
+malirrm <- data_in
+
+# export data -------------------------------------------------------------
+
+usethis::use_data(malirrm, overwrite = TRUE)
+
+# prepare dataset export files --------------------------------------------
+
+fs::dir_create(here::here("inst", "extdata"))
+write_csv(malirrm, here::here("inst", "extdata", "malirrm.csv"))
+openxlsx::write.xlsx(malirrm, here::here("inst", "extdata", "malirrm.xlsx"))
